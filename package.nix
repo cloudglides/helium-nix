7fd1179ab7e89c220828acf86ff7085bd73fc2a1
cloudglides <cloudglides@proton.me>
2026-03-11 23:12:07 +0530

    fixs: oopsies

--- package.nix
+++ package.nix
@@ -5,24 +5,39 @@
 }: let
   pname = "helium";
   version = "0.9.4.1";
+
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    sha256 = "1s4yhbzcmh9wwg5mnk19m72r48px7259vy0z4yfqpb2fxid1v61p";
+    hash = "sha256-N5gdWuxOrIudJx/4nYo4/SKSxakpTFvL4zzByv6Cnug=";
   };
+
   appimageContents = appimageTools.extractType2 {inherit pname version src;};
 in
   appimageTools.wrapType2 {
     inherit pname version src;
+
     extraInstallCommands = ''
-      install -m 444 -D ${appimageContents}/helium.desktop $out/share/applications/helium.desktop
+      # Install desktop file
+      install -Dm444 ${appimageContents}/helium.desktop \
+        $out/share/applications/helium.desktop
+
+      # Fix executable path
       substituteInPlace $out/share/applications/helium.desktop \
-        --replace-fail 'Exec=helium' 'Exec=$out/bin/helium'
-      cp -r ${appimageContents}/usr/share/icons $out/share/icons
-      sed -i '2i export CHROMIUM_FLAGS="--use-gl=egl $CHROMIUM_FLAGS"' $out/bin/helium
+        --replace 'Exec=AppRun' 'Exec=${pname}'
+
+      # Install icons if present
+      if [ -d ${appimageContents}/usr/share/icons ]; then
+        cp -r ${appimageContents}/usr/share/icons $out/share/
+      fi
+
+      # Chromium EGL fix
+      sed -i '2i export CHROMIUM_FLAGS="--use-gl=egl $CHROMIUM_FLAGS"' \
+        $out/bin/${pname}
     '';
+
     meta = with lib; {
-      homepage = "https://github.com/imputnet/helium-linux";
       description = "Helium web browser";
+      homepage = "https://github.com/imputnet/helium-linux";
       platforms = ["x86_64-linux"];
       mainProgram = "helium";
     };
