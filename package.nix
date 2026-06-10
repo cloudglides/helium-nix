f0e106b76874422dee45660df05c28b8b1003517
cloudglides <cloudglides@proton.me>
2026-06-11 01:29:13 +0530

    bump: 0.13.2.1

--- package.nix
+++ package.nix
@@ -4,11 +4,11 @@
   appimageTools,
 }: let
   pname = "helium";
-  version = "0.9.4.1";
+  version = "0.13.2.1";
 
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    hash = "sha256-N5gdWuxOrIudJx/4nYo4/SKSxakpTFvL4zzByv6Cnug=";
+    hash = "sha256-I9VqXE20FNjEz9FyvcCZ8ZqRZbPIU+QtGPblAdwJRk8=";
   };
 
   appimageContents = appimageTools.extractType2 {inherit pname version src;};
